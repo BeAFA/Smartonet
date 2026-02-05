aa99e406d78fa574775e7104a1c04c390f411837
BeAFA <pt281973555k@gmail.com>
2026-02-05 18:18:56 +0700

    update newest technologia

--- smartonet/test/widget_test.dart
+++ smartonet/test/widget_test.dart
@@ -1,6 +1,6 @@
-import 'package:smartonet/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:smartonet/screens/home_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
