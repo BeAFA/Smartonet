418f9b325dc22632115db0ab49dd84fce5f4c1f0
BeAFA <pt281973555k@gmail.com>
2026-03-07 08:59:02 +0700

    update music from user

--- smartonet/lib/main.dart
+++ smartonet/lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
 import 'services/alarm_service.dart';
 import 'services/audio_service.dart';
 import 'screens/home_screen.dart';
@@ -15,8 +17,24 @@ void main() async {
 
   await AudioService().init();
   await AppointmentService.init();
+  await _initAppFolders();
 
   final prefs = await SharedPreferences.getInstance();
   final bool seenOnboarding = prefs.getBool('seen_onboarding') ?? false;
   runApp(Smartonet(showOnboarding: !seenOnboarding));
+}
+
+Future<void> _initAppFolders() async {
+  try {
+    final directory = await getApplicationDocumentsDirectory();
+    final customSoundsPath = p.join(directory.path, 'Sounds', 'Customize');
+    final dir = Directory(customSoundsPath);
+
+    if (!await dir.exists()) {
+      await dir.create(recursive: true);
+      debugPrint("Đã tạo sẵn thư mục chứa nhạc cá nhân: $customSoundsPath");
+    }
+  } catch (e) {
+    debugPrint("Lỗi khi tạo thư mục: $e");
+  }
 }
